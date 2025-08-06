3feafc0eb580c05d6a3688838f1b0d54b2fd99a5
tavioribeiro <otavio123ribeiro@gmail.com>
2025-08-05 22:32:56 -0300

    ✨ Adicionados novos recursos de drawable e componente de botão com sublinhado na tela de calculadora 📜 Descrição: Adicionados novos arquivos de recurso drawable ('icon_folder.svg', 'icon_history.svg', 'icon_robot.svg') e um novo componente de botão com sublinhado ('UnderlineButton.kt'). Atualizada a tela de calculadora ('CalculatorScreen.kt') para incluir os novos botões e recursos.

--- composeApp/src/commonMain/kotlin/org/upxdev/calculatordpi/presentation/features/calculator/CalculatorScreen.kt
+++ composeApp/src/commonMain/kotlin/org/upxdev/calculatordpi/presentation/features/calculator/CalculatorScreen.kt
@@ -4,8 +4,10 @@ package org.upxdev.calculatordpi.presentation.features.calculator
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,17 +30,23 @@ import org.upxdev.calculatordpi.presentation.components.inputs.MidInput
 import org.upxdev.calculatordpi.theme.AppTheme
 import org.upxdev.calculatordpi.theme.ThemeState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import calculatordpi.composeapp.generated.resources.Res
 import calculatordpi.composeapp.generated.resources.icon_clear
+import calculatordpi.composeapp.generated.resources.icon_folder
+import calculatordpi.composeapp.generated.resources.icon_history
 import calculatordpi.composeapp.generated.resources.icon_plus
+import calculatordpi.composeapp.generated.resources.icon_robot
 import org.jetbrains.compose.resources.painterResource
 import org.upxdev.calculatordpi.presentation.components.buttons.IconTextButton
+import org.upxdev.calculatordpi.presentation.components.buttons.UnderlineButton
 
 
 @Composable
 fun CalculatorScreen() {
     var text by remember { mutableStateOf("") }
+    var selectedButton by remember { mutableStateOf("Projetos") }
 
     Column(
         modifier = Modifier
@@ -82,6 +91,43 @@ fun CalculatorScreen() {
                 modifier = Modifier.padding(16.dp)
             )
         }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .width(1232.dp)
+        ) {
+            UnderlineButton(
+                text = "Projetos",
+                icon = painterResource(Res.drawable.icon_folder),
+                isSelected = (selectedButton == "Projetos"),
+                onClick = { selectedButton = "Projetos" }
+            )
+
+            UnderlineButton(
+                text = "AI Agentes",
+                icon = painterResource(Res.drawable.icon_robot),
+                isSelected = (selectedButton == "AI Agentes"),
+                onClick = { selectedButton = "AI Agentes" }
+            )
+
+            UnderlineButton(
+                text = "Histórico",
+                icon = painterResource(Res.drawable.icon_history),
+                isSelected = (selectedButton == "Histórico"),
+                onClick = { selectedButton = "Histórico" }
+            )
+        }
+
+        Box(modifier = Modifier
+            .width(1232.dp)
+            .height(1.dp)
+            .background(AppTheme.colors.color5)
+        )
+
 
         MidInput(
             initialValue = text,
